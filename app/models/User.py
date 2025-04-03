76f7ba2a9f201c9a50b9d684c9470de72b9a97a8
andres <locogb26@gmail.com>
2025-04-03 15:55:02 -0500

    commit #1 - funciona

--- app/models/User.py
+++ app/models/User.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Usuario(db.Model):  # Capitalize 'Model'
+class Usuario(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     proyectos = db.relationship('Proyecto', backref="usuario", lazy=True)
