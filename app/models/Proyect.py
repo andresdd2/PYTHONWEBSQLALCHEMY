ea2bec9cbdf39dba77940296701aedae6393e3bc
andres <locogb26@gmail.com>
2025-04-03 15:51:52 -0500

    commit #1 - configuración

--- app/models/Proyect.py
+++ app/models/Proyect.py
@@ -2,8 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Proyecto(db.model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable = False)
-    usuario_id = db.Column(db.Integer, db.ForeignKey('Usuario.id'), nullable = False)
-    tareas = db.relationship('Tarea', backref="tarea", lazy=True)
+class Proyecto(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)  
+    tareas = db.relationship('Tarea', backref="proyecto", lazy=True)
--- app/models/Tarea.py
+++ app/models/Tarea.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Tarea(db.model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable = False)
-    pryecto_id = db.Column(db.Integer, db.ForeignKey('proyecto.id'), nullable = False)
+class Tarea(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    proyecto_id = db.Column(db.Integer, db.ForeignKey('proyecto.id'), nullable=False)
